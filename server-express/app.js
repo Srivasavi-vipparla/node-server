b93864b241fb315f7a4a7cce225ab83504eaabfe
Sri Vasavi <69984398+Srivasavi-vipparla@users.noreply.github.com>
2020-10-09 18:26:52 -0500

    add files

---
+++ server-express/app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+
+
+app.get('/', (req, res) => {
+    res.send(' Hey , i am  Sri Vasavi ..This is my express app ')
+  })
+  
+  app.listen(3333, () => {
+    console.log(`Listening on port 3333!`)
+  })
